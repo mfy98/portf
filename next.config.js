a5ff105e593442052af2fc363fde13413a7c9910
Mehmet Furkan Yiğit <74006145+mfy98@users.noreply.github.com>
2025-09-01 02:07:42 +0300

    s

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  experimental: {
-    typedRoutes: true
-  }
+  experimental: { typedRoutes: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
